ecf81f8fe5e02a70c81aa8487bd96b367acd26e7
SmartLtd <63488179+SmartLtd@users.noreply.github.com>
2025-06-25 16:52:09 +0100

    Update ec2.tf

--- sonar-scanner/src/terraform/aws/ec2.tf
+++ sonar-scanner/src/terraform/aws/ec2.tf
@@ -268,7 +268,7 @@ resource "aws_flow_log" "vpcflowlogs" {
   })
 }
 
-resource "aws_s3_bucket" "flowbucket" {
+resource "sonar-scanning-examples/sonar-scanner/src/docker/Dockerfile" {
   bucket        = "${local.resource_prefix.value}-flowlogs"
   force_destroy = true
 
